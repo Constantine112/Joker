c41ffd6ffb2bb4c05a601393baee85a8ab8abde0
Constantine112 <768131566@qq.com>
2018-05-01 21:49:21 +0800

    fig orderBuy

--- src/html/login/login.js
+++ src/html/login/login.js
@@ -1,16 +1,13 @@
 import Vue from 'vue'
 import App from './App'
-import router from './route'
+// import router from './route'
 import store from './store'
-import iView from 'iview'
-import 'iview/dist/styles/iview.css'
-Vue.use(iView)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
+  // router,
   store,
   template: '<App/>',
   components: { App }
